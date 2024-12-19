fdc8dbda13baf53d3ea3a73adbea07a5be1e926f
William Chen <wwilliamchenn@gmail.com>
2024-12-19 18:55:45 -0500

    fixed backend routing

--- webpage/src/questionnaire.js
+++ webpage/src/questionnaire.js
@@ -62,7 +62,7 @@ const Questionnaire = ({ setPersonalityResult }) => {
 
         // Send results to backend
         try {
-            const res = await fetch("http://localhost:5001/api/save-results", {
+            const res = await fetch("https://pbrm-backend.onrender.com/api/save-results", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
